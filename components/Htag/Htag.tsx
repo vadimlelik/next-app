b41898168d6514e95aec1b8d83b1fcdda891c4a0
vadim.cvirko <promiti90@gmail.com>
2022-12-02 14:51:13 +0300

    first commit

---
+++ components/Htag/Htag.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { HtagProps } from "./HtagProps";
+
+const Htag = ({ tag, children }: HtagProps): JSX.Element => {
+  return <>{children}</>;
+};
+
+export default Htag;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Htag from "../components/Htag/Htag";
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Htag tag="h1">Тут будет текс</Htag>
+    </div>
+  );
+}
