8e5d39012a646d21facb41d31e1c7f5cfd5d96ec
vadimcvirko <promiti90@gmail.com>
2022-12-07 13:53:52 +0300

    add footer grid

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import styles from "./Footer.module.css";
 import { FooterProps } from "./FooterProps";
+import cn from "classnames";
+import { format } from "date-fns";
 
-const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <div className={cn(className, styles.footer)} {...props}>
+      <div>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</div>
+      <a href="#">Пользовательское соглашение</a>
+      <a href="#">Политика конфиденциальности</a>
+    </div>
+  );
 };
 
 export default Footer;
