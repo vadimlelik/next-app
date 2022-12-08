d4a628e0e53b86f2fc2018bb657bad729d77968f
vadimcvirko <promiti90@gmail.com>
2022-12-08 12:42:57 +0300

    server side rendering

--- interfaces/page.interface.ts
+++ interfaces/page.interface.ts
@@ -6,9 +6,9 @@ export enum TopLevelCategory {
 }
 
 export interface TopPageAdvantage {
+  _id: string;
   title: string;
   description: string;
-  _id: string;
 }
 
 export interface HhData {
@@ -20,49 +20,20 @@ export interface HhData {
   updatedAt: Date;
 }
 
-export interface Blog {
-  h1: string;
-  metaTitle: string;
-  metaDescription: string;
-  views: number;
-  _id: string;
-}
-
-export interface Sravnikus {
-  metaTitle: string;
-  metaDescription: string;
-  qas: any[];
-  _id: string;
-}
-
-export interface Learningclub {
-  metaTitle: string;
-  metaDescription: string;
-  qas: any[];
-  _id: string;
-}
-
 export interface TopPageModel {
-  _id: string;
   tags: string[];
+  _id: string;
   secondCategory: string;
   alias: string;
   title: string;
   category: string;
-  seoText: string;
+  seoText?: string;
   tagsTitle: string;
   metaTitle: string;
   metaDescription: string;
   firstCategory: TopLevelCategory;
-  advantages: TopPageAdvantage[];
+  advantages?: TopPageAdvantage[];
   createdAt: Date;
   updatedAt: Date;
-  __v: number;
-  hh: HhData;
-  qas: any[];
-  addresses: any[];
-  categoryOn: string;
-  blog: Blog;
-  sravnikus: Sravnikus;
-  learningclub: Learningclub;
+  hh?: HhData;
 }
--- pages/courses/[alias].tsx
+++ pages/courses/[alias].tsx
@@ -1,38 +1,64 @@
-import axios from "axios";
-import { GetStaticProps } from "next";
-import { useState } from "react";
-import { MenuItem } from "../../interfaces/menu.interface";
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
+import React from "react";
 import { withLayout } from "../../layout/Layout";
+import axios from "axios";
+import { MenuItem } from "../../interfaces/menu.interface";
+import { TopPageModel } from "../../interfaces/page.interface";
+import { ParsedUrlQuery } from "node:querystring";
+import { ProductModel } from "../../interfaces/product.interface";
 
-function Course({ menu, firstCategory }: CourseProps): JSX.Element {
-  const [raiting, setRaiting] = useState<number>(4);
-  return (
-    <>
-      <span> тут будет текст </span>
-    </>
-  );
+const firstCategory = 0;
+
+function Course({ menu, page, products }: CourseProps): JSX.Element {
+  return <>{products && products.length}</>;
 }
 
 export default withLayout(Course);
 
-export const getStaticProps: GetStaticProps<CourseProps> = async () => {
-  const firstCategory = 0;
+export const getStaticPaths: GetStaticPaths = async () => {
   const { data: menu } = await axios.post<MenuItem[]>(
-    process.env.NEEXT_PUBLIC_DOMAIN + "/api/top-page/find",
-    {
-      firstCategory,
-    }
-  );
-  const { data: page } = await axios.post<MenuItem[]>(
-    process.env.NEEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
     {
       firstCategory,
     }
   );
   return {
+    paths: menu.flatMap((m) => m.pages.map((p) => "/courses/" + p.alias)),
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<CourseProps> = async ({
+  params,
+}: GetStaticPropsContext<ParsedUrlQuery>) => {
+  if (!params) {
+    return {
+      notFound: true,
+    };
+  }
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    {
+      firstCategory,
+    }
+  );
+  const { data: page } = await axios.get<TopPageModel>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/byAlias/" + params.alias
+  );
+  const { data: products } = await axios.post<ProductModel[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/product/find",
+    {
+      category: page.category,
+      limit: 10,
+    }
+  );
+
+  return {
     props: {
       menu,
       firstCategory,
+      page,
+      products,
     },
   };
 };
@@ -40,4 +66,6 @@ export const getStaticProps: GetStaticProps<CourseProps> = async () => {
 interface CourseProps extends Record<string, unknown> {
   menu: MenuItem[];
   firstCategory: number;
+  page: TopPageModel;
+  products: ProductModel[];
 }
