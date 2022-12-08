91b7c0e77fb9b4fe5a25a9e6419905a2662e6cde
vadimcvirko <promiti90@gmail.com>
2022-12-08 17:09:22 +0300

    add context

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -5,6 +5,7 @@ import { LayoutProps } from "./LayoutProps";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Sidebar from "./Sidebar/Sidebar";
+import { AppContextProvider, IAppContext } from "../context/app.Context";
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
@@ -17,14 +18,16 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
   );
 };
 
-export const withLayout = <T extends Record<string, unknown>>(
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(
   Component: FunctionComponent<T>
 ) => {
   return function withLayoutComponent(props: T): JSX.Element {
     return (
-      <Layout>
-        <Component {...props} />
-      </Layout>
+      <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      </AppContextProvider>
     );
   };
 };
