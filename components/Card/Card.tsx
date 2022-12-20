05a17d441fafbd30e1e06b795f63f79cebadf797
vadim.cvirko <promiti90@gmail.com>
2022-12-20 14:36:29 +0300

    card hhdata

---
+++ components/Card/Card.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { CardProps } from "./CardProps";
+import cn from "classnames";
+import styles from "./Card.module.css";
+
+const Card = ({
+  color = "white",
+  children,
+  className,
+  ...props
+}: CardProps): JSX.Element => {
+  return (
+    <div
+      className={cn(styles.card, className, {
+        [styles.blue]: color == "blue",
+      })}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
--- pages-component/TopPageComponent/TopPageComponents.tsx
+++ pages-component/TopPageComponent/TopPageComponents.tsx
@@ -1,11 +1,40 @@
+import Card from "../../components/Card/Card";
+import HhData from "../../components/HhData/HhData";
+import Htag from "../../components/Htag/Htag";
+import Tag from "../../components/Tag/Tag";
 import { TopPageComponentProps } from "./TopPage.component.props";
+import styles from "./TopPageComponent.module.css";
 
 const TopPageComponents = ({
   page,
   products,
   firstCategory,
 }: TopPageComponentProps): JSX.Element => {
-  return <>{products && products.length}</>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && (
+          <Tag color="grey" size="medium">
+            {products.length}
+          </Tag>
+        )}
+        <span>Sorted</span>
+      </div>
+      <div className={styles.product}>
+        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
+      </div>
+      <div className={styles.hhTitle}>
+        <Htag tag="h2"> Вакансии {page.category}</Htag>
+        {products && (
+          <Tag color="red" size="medium">
+            hh.ru
+          </Tag>
+        )}
+      </div>
+      <HhData {...page.hh} />
+    </div>
+  );
 };
 
 export default TopPageComponents;
