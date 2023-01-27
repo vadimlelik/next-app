076e32ae1a77540d70f0edd7abb53b0904675390
vadim.cvirko <promiti90@gmail.com>
2023-01-27 11:45:35 +0300

    Отзывы

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,32 +1,72 @@
-import React from "react";
-import {ProductProps} from "./ProductProps";
+import React, { useState } from "react";
+import { ProductProps } from "./ProductProps";
 import cn from "classnames";
 import styles from "./Product.module.css";
 import Card from "../Card/Card";
 import Rating from "../Rating/Rating";
 import Tag from "../Tag/Tag";
+import { declOfNum, priceRu } from "../../helpers/helpers";
+import Review from "../Review/Review";
 
-const Product = ({product, className, ...props}: ProductProps): JSX.Element => {
-        return (<Card className={cn(styles.product)}>
-            <div className={styles.logo}>
-                <img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title}/>
-            </div>
-            <div className={styles.title}>{product.title}</div>
-            <div className={styles.price}>
-                {product.price}
-            </div>
-            <div className={styles.credit}>{product.credit}</div>
-            <div className={styles.rating}>
-                <Rating raiting={product.reviewAvg ?? product.initialRating}/>
-            </div>
-            <div className={styles.tags}>
-                {product.categories.map((c) => (<Tag key={c} className={styles.category} color='ghost'>{c}</Tag>))}
-            </div>
-            <div className={styles.priceTitle}> {product.price}</div>
-            <div className={styles.creditTitle}> {product.credit}</div>
-            <div className={styles.rateTitle}> {product.reviewCount} Отзывов</div>
-        </Card>);
-    }
-;
-
+const Product = ({
+    product,
+    className,
+    ...props
+}: ProductProps): JSX.Element => {
+    const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false);
+    return (
+        <>
+            <Card className={cn(styles.product)}>
+                <div className={styles.logo}>
+                    <img
+                        src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+                        alt={product.title}
+                    />
+                </div>
+                <div className={styles.title}>{product.title}</div>
+                <div className={styles.price}>
+                    {priceRu(product.price)}
+                    {product.oldPrice && (
+                        <Tag className={styles.oldPrice} color="green">
+                            {priceRu(product.price - product.oldPrice)}
+                        </Tag>
+                    )}
+                </div>
+                <div className={styles.credit}>{product.credit}</div>
+                <div className={styles.rating}>
+                    <Rating
+                        raiting={product.reviewAvg ?? product.initialRating}
+                    />
+                </div>
+                <div className={styles.tags}>
+                    {product.categories.map((c) => (
+                        <Tag key={c} className={styles.category} color="ghost">
+                            {c}
+                        </Tag>
+                    ))}
+                </div>
+                <div className={styles.priceTitle}> {product.price}</div>
+                <div className={styles.creditTitle}> {product.credit}</div>
+                <div className={styles.rateTitle}>
+                    {declOfNum(product.reviewCount, [
+                        "отзыв",
+                        "отзыва",
+                        "отзывов",
+                    ])}
+                </div>
+            </Card>
+            <Card
+                color="blue"
+                className={cn(styles.reviews, {
+                    [styles.opened]: isReviewOpened,
+                    [styles.closed]: !isReviewOpened,
+                })}
+            >
+                {product.reviews.map((r) => (
+                    <Review key={r._id} review={r} />
+                ))}
+            </Card>
+        </>
+    );
+};
 export default Product;
--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -6,34 +6,46 @@ import { FirstLevelMenuItem } from "../interfaces/menu.interface";
 import { TopLevelCategory } from "../interfaces/page.interface";
 
 export const firstLevelMenu: FirstLevelMenuItem[] = [
-  {
-    route: "courses",
-    name: "Курсы",
-    icon: <CoursesIcon />,
-    id: TopLevelCategory.Courses,
-  },
-  {
-    route: "courses",
-    name: "Книги",
-    icon: <BooksIcon />,
-    id: TopLevelCategory.Books,
-  },
-  {
-    route: "courses",
-    name: "Сервисы",
-    icon: <ServicesIcon />,
-    id: TopLevelCategory.Services,
-  },
-  {
-    route: "courses",
-    name: "Товары",
-    icon: <ProductsIcon />,
-    id: TopLevelCategory.Products,
-  },
+    {
+        route: "courses",
+        name: "Курсы",
+        icon: <CoursesIcon />,
+        id: TopLevelCategory.Courses,
+    },
+    {
+        route: "courses",
+        name: "Книги",
+        icon: <BooksIcon />,
+        id: TopLevelCategory.Books,
+    },
+    {
+        route: "courses",
+        name: "Сервисы",
+        icon: <ServicesIcon />,
+        id: TopLevelCategory.Services,
+    },
+    {
+        route: "courses",
+        name: "Товары",
+        icon: <ProductsIcon />,
+        id: TopLevelCategory.Products,
+    },
 ];
 
 export const priceRu = (price: number): string =>
-  price
-    .toString()
-    .replace(/\B(?=(\d{3})+(?!\d))/g, " ")
-    .concat(" ₽");
+    price
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+        .concat(" ₽");
+
+export const declOfNum = (
+    number: number,
+    titles: [string, string, string]
+): string => {
+    const cases = [2, 0, 1, 1, 1, 2];
+    return titles[
+        number % 100 > 4 && number % 100 < 20
+            ? 2
+            : cases[number % 10 < 5 ? number % 10 : 5]
+    ];
+};
