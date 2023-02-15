86a8bc2c51262b11b5251fae8501a7112c1bdd1a
vadimcvirko <promiti90@gmail.com>
2023-02-15 15:22:31 +0300

    .

--- components/Review/Review.tsx
+++ components/Review/Review.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { PProps } from "./PProps";
+import cn from "classnames";
+import styles from "./P.module.css";
 
-const Review = () => {
-    return <div>Review</div>;
+const P = ({
+  size = "middle",
+  children,
+  className,
+  ...props
+}: PProps): JSX.Element => {
+  return (
+    <p
+      className={cn(styles.p, className, {
+        [styles.small]: size === "small",
+        [styles.middle]: size === "middle",
+        [styles.large]: size === "large",
+      })}
+      {...props}
+    >
+      {children}
+    </p>
+  );
 };
 
-export default Review;
+export default P;
